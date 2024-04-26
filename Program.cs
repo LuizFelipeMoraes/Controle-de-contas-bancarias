94289ca7bcd9f82bd1cedd5d64888648b3eb9a55
LuizFelipeMoraes <166819529+LuizFelipeMoraes@users.noreply.github.com>
2024-04-26 08:50:51 -0300

    controle de contas bancarias
    projeto de controle de contras bancarias

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿﻿namespace Controle_de_contas_bancarias;
+
+class Program
+{
+	static void Main()
+	{
+		ContaPoupanca contaPoupanca = new ContaPoupanca { NumeroDaConta = 36446, Saldo = 23654, TaxaDeJuros = 0.08 };
+		ContaCorrente contaCorrente = new ContaCorrente(082267, 2289, 9);
+
+		contaPoupanca.Depositar(840);
+		contaPoupanca.Sacar(3980);
+		contaPoupanca.AplicarJuros();
+
+		contaCorrente.Depositar(4000);
+		contaCorrente.Sacar(870);
+		
+		Console.WriteLine("Saldo da Poupança: " + contaPoupanca.Saldo);
+		Console.WriteLine("Saldo da Corrente: " + contaCorrente.Saldo);
+	}
+}
